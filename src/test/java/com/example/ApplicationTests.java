f56d93b5cf39a043fa9075038dfcd4ca33768dc7
wangtao <wtvllx@qq.com>
2018-11-26 11:39:56 +0800

    updateActiveUser测试用例修改

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={Application.class})
 public class ApplicationTests {
@@ -17,7 +19,7 @@ public class ApplicationTests {
     public  void  esServiceTest(){
 
         try {
-            esService.updateActiveUser("18920378921","2018-11-26 10:22:22");
+            esService.updateActiveUser("18920378921",new Date());
         } catch (Exception e) {
             e.printStackTrace();
         }
