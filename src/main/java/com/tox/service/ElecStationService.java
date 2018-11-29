98f0ab294cef45f38cb73dd79e94c4e1a54a77cb
wangtao <wtvllx@qq.com>
2018-11-29 15:30:25 +0800

    updateActiveUser结束日期逻辑修改

--- src/main/java/com/tox/service/ElecStationService.java
+++ src/main/java/com/tox/service/ElecStationService.java
@@ -56,7 +56,7 @@ public class ElecStationService {
                 onlineCal.setTime(online);
                 Integer onlineDay= onlineCal.get(Calendar.DAY_OF_MONTH);
                 Date endDate= null;
-                if(day>=onlineDay){
+                if(onlineDay>=day){
                     onlineCal.add(Calendar.MONTH,count);
                 }else{
                     onlineCal.add(Calendar.MONTH,count-1);
